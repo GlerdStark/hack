44cbd9e5e852969c4cc800ea2d483960a9b75f21
Glerd Stark <Love_in_Decembers@163.com>
2016-10-07 19:19:02 +0800

    shit

--- hack.c
+++ hack.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
-#define PRAISE "You are an extraordinary being"
+#include<string.h>
+#define PRAISE "You are an extraordinary"
 int main(void)
 {
     char name[40];
 
     printf("What's your name?");
     scanf("%s",name);
-    printf("Hello,%s.%s\n",name,PRAISE);
+    printf("Hell,%s.%s\n",name,PRAISE);
+    printf("Your name of %Lu letters occupies %u memory cells.\n",
+           strlen(name),sizeof name);
+    printf("The phrase of phrase has %Lu letters",strlen(PRAISE));
+    printf("and occupies %Lu memory cells.\n",sizeof PRAISE);
 
     return 0;
 
