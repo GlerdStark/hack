12fa36c896c02eedf59a430f0fb4df80a77e2649
Glerd Stark <Love_in_Decembers@163.com>
2016-10-09 17:59:54 +0800

    i like programming

--- hack.c
+++ hack.c
@@ -1,42 +1,22 @@
-#include <stdio.h>
-const int S_PER_M = 60;          //一分钟的秒数
-const int S_PER_H = 3600;
-const double M_PER_K = 0.62137;
+#include<stdio.h>
 int main(void)
 {
-    double distk,distm;
-    double rate;
-    int min ,sec;
-    int time;
-    double mtime;
-    int mmin,msec;
+    long num;
+    long sum = 0L;
+    int status;
 
-    printf("This program converts your time for a metric race\n");
-    printf("to a time for running a mile and to your average\n");
-    printf("speed in miles per hour.\n");
-    printf("Please enter , in kilometers ,the distance run.\n");
-    scanf("%lf",&distk);
-    printf("Next enter the time in minutes and seconds.\n");
-    printf("Begin by entering the minutes.\n");
-    scanf("%d",&min);
-    printf("Now enter the seconds.\n");
-    scanf("%d",&sec);
+    printf("Please enter an integer to be summed ");
+    printf("(q to quit):");
+    status = scanf("%ld",&num);
+    while (status ==1)
+    {
+        sum = sum + num;
+        printf("Please ener next integer (q to quit):");
+        status = scanf("%ld",&num);
 
-    time =  S_PER_M * min + sec;
-    distm = M_PER_K *distk;
-    rate = distm /time * S_PER_H;
-    mtime =(double)time / distm;
-    mmin = (int)mtime / S_PER_M;
-    msec = (int)mtime % S_PER_M;
-
-    printf("You ran %1.2f km (%1.2f miles) in %d min,%d sec.\n",
-           distk,distm,min,sec);
-    printf("That pace corresponds to running a mile in %d min,",
-           mmin);
-    printf("%d sec.\n Your average speed was %1.2f mph .\n",msec,
-           rate);
+    }
+    printf("Those integers sum to %ld.\n",sum);
 
     return 0;
-
 
 }
