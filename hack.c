f2f8652e5936f6d977fe02fb439f76baa294d903
Glerd Stark <Love_in_Decembers@163.com>
2016-10-09 19:26:10 +0800

    postage.c

--- hack.c
+++ hack.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int num;
+    const int FIRST_OZ = 46;
+    const int NEXT_OZ = 20;
+    int ounces, cost;
 
-    printf("    n   n cubed\n");
-    for (num = 1;num <= 6; num++)
-        printf("%5d %5d\n",num,num*num*num);
+    printf(" ounces  cost\n");
+    for (ounces = 1,cost = FIRST_OZ;ounces <= 16; ounces++,cost += NEXT_OZ)
+        printf("%5d  $%4.2f\n", ounces, cost / 100.0);
 
     return 0;
+
 }
