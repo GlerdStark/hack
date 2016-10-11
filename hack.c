a6b89811462c38e828298ad963dbcb68c952c6a0
Glerd Stak <Love_in_Decembers@163.com>
2016-10-11 18:26:50 +0800

    my creation

--- hack.c
+++ hack.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
-double power(double n, int p);
+#define SIZE 8
 int main(void)
 {
-    double x, xpow;
-    int exp;
+    int by_twos[SIZE];
+    int index;
 
-    printf("Enter a number and the positive integer power");
-    printf(" to which\nthe number will be raised, Enter q");
-    printf(" to quit.\n");
-    while (scanf("%lf%d", &x, &exp) == 2)
+    for (index = 1; index <= SIZE; index++)
     {
-        xpow = power(x,exp);
-        printf("%.3g to the power %d is %.5g\n", x, exp, xpow);
-        printf("Enter next pair of numbers or q to quit.\n");
+        by_twos[index - 1] = 2 * index;
+        printf("%d\n", by_twos[index - 1]);
 
     }
-    printf("Hope you enjoyed this power trip --bye!\n");
 
     return 0;
-
-}
-
-double power(double n, int p)
-{
-    double pow = 1;
-    int i;
-
-    for (i = 1; i <= p; i++)
-        pow *= n;
-
-    return pow;
 
 }
