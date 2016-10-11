3ea4087a0f82b7a01036c7fec7c07d9ae6036af3
Glerd Stak <Love_in_Decembers@163.com>
2016-10-11 19:03:40 +0800

    colddays.c

--- hack.c
+++ hack.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 int main(void)
 {
-    int k;
+    const int FREEZING = 0;
+    float temperature;
+    int cold_days = 0;
+    int all_days = 0;
 
-    for (k = 1,printf("%d: Hi!\n",k);printf("k = %d\n",k),
-        k*k < 26; k += 2, printf("Now k is %d\n",k))
-            printf("k is %d in the loop\n",k);
+    printf("Enter the list of daily low temperatures.\n");
+    printf("Use Celsius, and enter q to quit.\n");
+    while (scanf("%f", &temperature) == 1)
+    {
+        all_days++;
+        if (temperature < FREEZING)
+            cold_days++;
+    }
+    if (all_days != 0)
+        printf("%d days total: %.1f%% were below freezing.\n",
+               all_days, 100.0 * (float) cold_days / all_days);
+    if (all_days == 0)
+        printf("No data entered!\n");
 
     return 0;
-
 }
